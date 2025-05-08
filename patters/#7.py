7007a293e96d1d159ab21331a4620647f14b9661
Md Kaif Ansari <amdkaif843@gmail.com>
2025-05-08 20:13:12 +0530

    Add printWholePyramid function to display a whole pyramid pattern

---
+++ patters/#7.py
@@ -0,0 +1,26 @@
+def printWholePyramid(n):
+    
+    for i in range(n):
+        space = " " * (n-i-1)
+        stars = "*" *(2*i + 1)
+        space = " " * (n-i-1)
+        print( space + stars + space)
+        
+    
+    for i in range(n,0, -1):
+        space = " " * (n-i)
+        stars = "*" *(2*i - 1)
+        space = " " * (n-i)
+        print( space + stars + space)
+        
+printWholePyramid(5)
+#     *    
+#    ***   
+#   *****  
+#  ******* 
+# *********
+# *********
+#  ******* 
+#   *****  
+#    ***   
+#     *    
