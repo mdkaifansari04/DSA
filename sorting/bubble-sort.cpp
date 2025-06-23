0126f5260c4f9bb0768fd59f472d5e2a67528d4b
Md Kaif Ansari <amdkaif843@gmail.com>
2025-06-23 22:32:33 +0530

    feat: bubble sort

---
+++ sorting/bubble-sort.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int size){
+    int pos=0;
+    while(pos<size){
+        for (int i=0; i<size-pos; i++){
+            if(arr[i] > arr[i+1]){
+                swap(arr[i], arr[i+1]);
+            }
+        }
+        pos++;
+    }
+
+}
+int main(){
+
+    int arr [] = {12,34,4,6,1, 100,89, 23,4, 9};
+    int size = 10;
+    bubbleSort(arr,size);
+    for (int i=0; i<size; i++){
+        cout << arr[i] << " ";
+    }
+
+ }
