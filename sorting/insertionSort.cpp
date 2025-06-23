9100b80fafe9b1861b3ab2439d158e25ca5ffddc
Md Kaif Ansari <amdkaif843@gmail.com>
2025-06-23 22:55:35 +0530

    feat: insertion sort

---
+++ sorting/insertionSort.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+void insertionSort(int arr[], int size){
+    for (int i=0; i<size;i++){
+        int j=i;
+        while(j>0 && arr[j-1] > arr[j]){
+            swap(arr[j], arr[j-1]);
+            j--;
+        }
+    }
+}
+int main(){
+
+    int arr [] = {12,34,4,6,1, 100,89, 23,4, 9};
+    int size = 10;
+    insertionSort(arr,size);
+    for (int i=0; i<size; i++){
+        cout << arr[i] << " ";
+    }
+}
