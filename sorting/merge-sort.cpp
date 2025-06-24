68e226a99b26cbe01ca07ad646e0784d2a74941d
Md Kaif Ansari <amdkaif843@gmail.com>
2025-06-24 22:21:12 +0530

    feat: update merge sort

--- sorting/merge-sort.cpp
+++ sorting/merge-sort.cpp
@@ -1,13 +1,56 @@
 #include<iostream>
 using namespace std;
 
-void mergeSort(int arr[], int low, int high)
-{
-    if(low >=high) return;
-    
+void merge(int arr[], int low, int mid , int high){
+
+    int left = low;
+    int right = mid+1;
+    vector<int> temp;
+
+    while(left <= mid && right<=high){
+        if(arr[left]<arr[right]){
+            temp.push_back(arr[left]);
+            left++;   
+        }
+        else{
+            temp.push_back(arr[right]);
+            right++;   
+        }
+    }
+
+    while (left<=mid)
+    {
+        temp.push_back(arr[left]);
+        left++;   
+    }
+
+    while (right<=high)
+    {
+        temp.push_back(arr[right]);
+        right++;   
+    }
+    for(int i=low; i<=high; i++){
+        arr[i] = temp[i-low];
+    }
 }
 
-int main()
-{
+void mergeSort(int arr[], int low, int high) {
+    if (low >= high) return;
 
+    int mid = (low + high) / 2;
+
+    mergeSort(arr, low, mid);
+    mergeSort(arr, mid + 1, high);
+    merge(arr, low, mid, high);
+}
+
+int main() {
+    int arr[] = {12, 3, 5, 6, 1, 10};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    mergeSort(arr, 0, size - 1);
+
+    for (int i : arr) {
+        cout << i << " ";
+    }
 }
