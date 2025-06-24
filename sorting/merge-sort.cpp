138a76e6e04fa264620785b921f2cdb993d17598
Md Kaif Ansari <amdkaif843@gmail.com>
2025-06-24 22:29:29 +0530

    chore: add desc for merge sort

--- sorting/merge-sort.cpp
+++ sorting/merge-sort.cpp
@@ -1,6 +1,17 @@
 #include<iostream>
 using namespace std;
 
+/*
+
+Merge sort: it is the method where we divide the array and merge it in term of sorted order. 
+
+step 1: first we call recursive to the same fn for dividing and having a base condition as (when low == high then return)
+step 2: after both the arrays are splited and merge with checking the highest and lowest element after it. (this way a that splited part will get sorted and so on for others too)
+step 3: the merge fn have a temp array which basically have the sorted array of two un-sorted array, 
+
+
+*/
+
 void merge(int arr[], int low, int mid , int high){
 
     int left = low;
