6b09552bbe8e2bff6d7a96563a8e9ffe96cecfcf
Md Kaif Ansari <amdkaif843@gmail.com>
2025-07-03 16:05:32 +0530

    feat: better using hashing

--- array/medium/2-sum.cpp
+++ array/medium/2-sum.cpp
@@ -41,10 +41,36 @@ bool exitsTwoSumBetter (vector<int> &nums, int k){
     return false;
 }
 
-bool exitsTwoSumBetter2(vector<int> &nums, int k)
+int maxElement (vector<int> &nums){
+    int max = nums[0];
+    for (int i=0; i<nums.size();i++){
+        if(max < nums[i]){
+            max = nums[i];
+        }
+    }
+    return max;
+}
+bool exitsTwoSumBetter2(vector<int> &nums, int k){
+    int n = nums.size()+1;
+    vector<int> hashArr(maxElement(nums)+1, 0);
+
+    for (int i=0; i<n; i++){
+        hashArr[nums[i]] ++;
+    }
+
+    for(int i=0; i<n; i++){
+        int sub = k-nums[i];
+        if(sub>=0 && sub < hashArr.size() && hashArr[sub]){
+            return true;
+            break;
+        }
+    }
+    return false;
+}
 
 int main(){ 
     vector<int>arr={2,7,11,15};
-    bool result = exitsTwoSumBetter(arr, 22);
+    // bool result = exitsTwoSumBetter(arr, 22);
+    bool result = exitsTwoSumBetter2(arr, 25);
     cout << result;
 }
