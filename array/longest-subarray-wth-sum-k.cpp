8505f833e1a84829b33611ca8aa0dc66cd2958b2
Md Kaif Ansari <amdkaif843@gmail.com>
2025-07-03 15:37:08 +0530

    feat: longest subarray optimal

--- array/longest-subarray-wth-sum-k.cpp
+++ array/longest-subarray-wth-sum-k.cpp
@@ -33,13 +33,64 @@ vector<int> longestSubArrayBrute(vector<int> &nums, int k){
    return longestArray;
 }   
 
+vector<int> longestSubArrayBetter(vector<int> &nums, int k){
+    int n = nums.size();
+    int start =0;
+    int end = 0;
+    vector<int> longestSubArray;
+    int sum = 0;
+    for (int i=0; i<n; i++){
+        sum +=nums[i];
+    }
+    int x = 0;
+    // for(int i=0; i<n; i++){
+    //     x+=nums[i];
+    //     if(sum-x == k){
+    //         start=ibr;
+    //         end = n-1;
+    //         break;
+    //     }
+    // }
 
-vector<int> longestSubArrayOptimal(vector<int> &nums, int k){
-    
+    for (int i=start; i<=end; i++){
+        longestSubArray.push_back(nums[i]);
+        cout << nums[i] << " ";
+    }
+    return longestSubArray;
 }   
+
+// time complexity : O(2n)
+// space complexity : O(1)
+
+int longestSubArrayOptimal(vector<int> &nums, int k){
+    int n=nums.size();
+    int i=0;
+    int j=0;
+
+    int maxLength =0;
+    int sum = 0;
+
+    while (j<n)
+    {
+        sum+=nums[j];
+        j++;
+
+        if(sum==k){
+            maxLength = max(maxLength , j-i);
+        }
+        while(sum>k && i<j){
+            sum-=nums[i];
+            i++;
+        }
+    }
+    return maxLength;   
+}
 
 
 int main(){
     vector<int>arr={1,2,3,1,2,1,1,1,1,1,5,0};
-    vector<int> subArr = longestSubArrayBrute(arr, 5);
+    // vector<int> subArr = longestSubArrayBrute(arr, 5);
+    // vector<int> subArr = longestSubArrayBetter(arr, 5);
+    int length = longestSubArrayOptimal(arr, 5);
+    cout<< length << endl;
 }
