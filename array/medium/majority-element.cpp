60339c217722e38ec3c1f0e909b2bce7b5775f4b
Md Kaif Ansari <amdkaif843@gmail.com>
2025-07-06 20:06:43 +0530

    feat : implement majority element optimal approach

--- array/medium/majority-element.cpp
+++ array/medium/majority-element.cpp
@@ -1,4 +1,3 @@
-
 #include<iostream>
 #include<map>
 using namespace std;
@@ -7,22 +6,53 @@ using namespace std;
 //! NOTE: Whenever you think about the brute approach try to solve it using just simple loops and dont use any Data structure, and it is asked to optimize then go for other approaches
 
 // better force approach (what you though would be the brute is a better one)
-// time complexity : O(2n) == O(n)
+// time complexity :O(nlogn) + O(n)
 // space complexity : average case- O(n log n), worst case - O(n)
-int getMajorityElement(vector<int> &nums){
+int getMajorityElementBrute(vector<int> &nums){
     int n= nums.size();
     map<int, int> hashMap;
     int majorityElement = 0;
+
+    // this will iterate through O(n) times
     for (int i=0; i<n;i++){
         hashMap[nums[i]] +=1;
     }
-
-     for(auto i :hashMap){
+    
+    // we are iterating for O(N log N) times so we need get 
+    for(auto i :hashMap){
         if(i.second > (n/2)) {
             majorityElement = i.first;
         }
     }
     return majorityElement;
+}
+// optimal approach : using Moore's voting algorithm 
+//* Def: The algorithm works on the principle of where we took the starting element and then checks the other element,
+//* if the element is present after then increase the count : if ele = 3 and next we have 3342 (count ++ )
+//* if the element is not present after that decrement the count if ele = 3 next we have 4321 (count -- )
+//* if the count is 0 then we assign the element to ele (var)
+
+// and checks for further condition.
+// time complexity : O(n)
+// space complexity : O(1)
+
+int getMajorityElement(vector<int> &nums){
+    int n = nums.size();
+    int count=0;
+    int ele;
+
+    for(int i=0;i<n;i++){
+        if(count == 0){
+            ele = nums[i];
+            count++;
+        }
+        if(nums[i] == ele){
+            count++;
+        }else{
+            count--;
+        }
+    }
+    return ele;
 }
 
 int main(){ 
