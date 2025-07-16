a3cb39101435a218626f53511e8524b46b870683
Md Kaif Ansari <amdkaif843@gmail.com>
2025-07-16 23:07:07 +0530

    chore: implement correct next_permutation inbuilt

--- array/medium/next-permutaion.cpp
+++ array/medium/next-permutaion.cpp
@@ -6,21 +6,14 @@ int fact(int n){
     if(n==1) return 1;
     return n * fact(n-1);
 }
+
 vector<int> getNextPermutation(vector<int> &nums){
-    int n = nums.size();
-    int f = fact(n);
-    vector<int>temp;
-    bool c;
-    for(int i=0; i<f; i++){
-        c=next_permutation(nums.begin(), nums.end());
-        if(c) cout << "true" << endl;
-        else cout << "false" << endl;
-    }
-    for(auto i:temp) cout << i << " ";
-    return temp;
+    next_permutation(nums.begin(), nums.end());
+    return nums;
 }
 
 int main (){
-    vector<int> arr = {3,1,2};
-    getNextPermutation(arr);
+    vector<int> arr = {1,2,3};
+    vector<int> permuteArr = getNextPermutation(arr);
+    for(auto i:arr) cout << i << " ";
 }
