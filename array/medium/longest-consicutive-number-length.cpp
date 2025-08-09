97c37343a83e3dee6096ad0c8b58988265b2171e
Md Kaif Ansari <amdkaif843@gmail.com>
2025-08-09 15:22:58 +0530

    feat: implemented longest consecutive

--- array/medium/longest-consicutive-number-length.cpp
+++ array/medium/longest-consicutive-number-length.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 #include<algorithm>
-
+#include<unordered_set>
 using namespace std;
 
 bool linearSearch (vector <int> &arr, int num){
@@ -33,9 +33,8 @@ int longestConsecutiveNumberLengthBrute(vector <int> &nums){
 }
 
 //better approach 
-// time complexity : O(n) + O(n log n)
+// time complexity : O(n) + O(n log n). So O(n log n) + O(n) simplifies to O(n log n).
 // space complexity : O(1)
-
 int longestConsecutiveNumberLengthBetter(vector <int> &nums){
     if(nums.empty()) return 0;
     sort(nums.begin(), nums.end());
@@ -56,12 +55,42 @@ int longestConsecutiveNumberLengthBetter(vector <int> &nums){
     return longest;
 }
 
+// optimal approach 
+// time complexity : O(n) + O(n) == O(n)
+// space complexity : closer to O(n) in worst case this is O(n)
+
+int longestConsecutiveNumberLengthOptimal (vector<int> &nums){
+    // if no element
+    if(nums.empty()) return 0;
+    
+    int n = nums.size();
+    unordered_set<int> s(nums.begin(), nums.end());
+    int count = 0;
+    int longest = 0;
+
+    // looping the set
+    for(auto i:s){
+
+        // finding if there is no element before this element, then consider this as starting point and check for values after it
+        if(s.find(i-1) == s.end()){
+            count = 1;
+            int upcomingValue = 1;
+            while(s.find(i+upcomingValue) != s.end()){
+                count+=1;
+                upcomingValue +=1;
+            }
+            longest = max(count, longest);
+        }
+    }
+
+    return longest;
+}
 
 
 int main(){
     vector<int>arr={100,102,100,101,101,4,3,2,3,2,1,1,1,2};
 
-    int n = longestConsecutiveNumberLengthBetter(arr);
+    int n = longestConsecutiveNumberLengthOptimal(arr);
     cout<< endl;
     cout << n ;
 }
