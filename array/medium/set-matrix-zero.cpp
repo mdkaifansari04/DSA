e61ee295197d68fbd9e855e76d5196d75786a267
Md Kaif Ansari <amdkaif843@gmail.com>
2025-08-11 19:26:59 +0530

    chore: implemented optimal solution

--- array/medium/set-matrix-zero.cpp
+++ array/medium/set-matrix-zero.cpp
@@ -47,7 +47,7 @@ void setMatrixToZeroBetter(vector<vector<int>> &nums, int rows, int cols){
         for(int j=0; j<cols; j++){
             if(nums[i][j] == 0) {
                 rowsToBeZero[i] = 1;
-                colToBeZero[i] = 1;
+                colToBeZero[j] = 1;
             }
         }
     }
@@ -62,16 +62,55 @@ void setMatrixToZeroBetter(vector<vector<int>> &nums, int rows, int cols){
 
 }
 
+void setMatrixToZeroOptimal(vector<vector<int>> &matrix, int m, int n){
+    //  vector<int>rowsToBeZero(rows, 0),colToBeZero(cols, 0); --> rowTO
+    // row = matrix[0][...]
+    // cols = matrix[...][0]
+    // int col0 = 1;
+    int col0 = 1;
+
+    // checking and marking the 0th row and 0th col
+    for (int i=0; i<m; i++){
+        for (int j=0; j<n;j++){
+            if(matrix[i][j] == 0){
+                matrix[i][0] = 0;
+                if(j!=0)matrix[0][j] = 0;
+                else col0=0;
+            }
+         }
+    }
+
+    // conversion of the element. 
+    for (int i=1; i<m; i++){
+        for(int j=1; j<n; j++){
+            if(matrix[i][j] != 0){
+                if( matrix[0][j] ==0 || matrix[i][0] ==0){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    // checking for the 0th row
+    if(matrix[0][0] == 0){
+        for (int j=0; j<m; j++) matrix[0][j] = 0;
+    }
+
+    if(col0 == 0){
+         for (int i=0; i<m; i++) matrix[i][0] =0;
+    }
+
+}
+
 
 int main(){
-    int rows=4, cols = 4;
+    int rows=3, cols = 4;
     vector<vector<int>> arr = {
-    {1, 1, 1,1},
-    {1, 0, 0,1},
-    {1, 1, 0,1},
-    {1, 1, 1,1},
+    {0,1,2,0},
+    {3,4,5,2},
+    {1,3,1,5},
     };
-    setMatrixToZeroBetter(arr, rows, cols);
+    setMatrixToZeroOptimal(arr, rows, cols);
     for (int i=0; i<rows; i++){
         for (int j=0; j<cols; j++){
             cout << arr[i][j] << " " ;
